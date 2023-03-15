1fb4e652acdbb2f9cce9e0dff81248f573901d66
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-03-15 19:16:53 +0000

    Commit changes for version 0.2.1

---
+++ Sources/CheckoutCardManagementStub/LoggingSupport/LogEvent.swift
@@ -0,0 +1,41 @@
+//
+//  LogEvent.swift
+//  
+//
+//  Created by Alex Ioja-Yang on 12/09/2022.
+//
+
+import Foundation
+import CheckoutCardNetworkStub
+import CheckoutEventLoggerKit
+
+/// Analytics event wrappers
+enum LogEvent {
+    
+    /// Describe an initialisation of the CardManager
+    case initialized(design: CardManagementDesignSystem)
+ 
+    /// Describe a successful retrieval of the card list
+    case cardList(idSuffixes: [String])
+    
+    /// Describe a successful call to retrieve a pin
+    case getPin(idLast4: String, cardState: CardState)
+    
+    /// Describe a successful call to retrieve a card number
+    case getPan(idLast4: String, cardState: CardState)
+    
+    /// Describe a successful call to retrieve a security code
+    case getCVV(idLast4: String, cardState: CardState)
+    
+    /// Describe a successful call to retrieve a pan and a security code
+    case getPanCVV(idLast4: String, cardState: CardState)
+    
+    /// Describe a successfull event where a card state change was completed
+    case stateManagement(idLast4: String, originalState: CardState, requestedState: CardState, reason: String?)
+    
+    /// Describe a Push Provisioning event
+    case pushProvisioning(last4CardID: String, last4CardholderID: String)
+    
+    /// Describe an unexpected but non critical failure
+    case failure(source: String, error: Error)
+}
---
+++ Sources/CheckoutCardManagementStub/Models/Typealiases.swift
@@ -0,0 +1,24 @@
+//
+//  Typealiases.swift
+//
+//
+//  Created by Alex Ioja-Yang on 08/06/2022.
+//
+
+import Foundation
+import CheckoutCardNetworkStub
+
+/// Expiry date for a card
+public typealias CardExpiryDate = CheckoutCardNetworkStub.CardExpiryDate
+
+/// State for a card
+public typealias CardState = CheckoutCardNetworkStub.CardState
+
+/// Reason for requesting to perform a Card Suspend operation on card
+public typealias CardSuspendReason = CheckoutCardNetworkStub.CardSuspendReason
+
+/// Reason for requesting to perform a Card Revoke operation on card
+public typealias CardRevokeReason = CheckoutCardNetworkStub.CardRevokeReason
+
+/// Configuration object used for Push Provisioning
+public typealias ProvisioningConfiguration = CheckoutCardNetworkStub.ProvisioningConfiguration
