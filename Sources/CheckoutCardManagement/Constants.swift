f7bd539f6b7a27e7666434cc7ea453f8d83879a7
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-06-14 13:59:12 +0000

    Commit open source code changes for version 1.0.1

--- Sources/CheckoutCardManagement/Constants.swift
+++ Sources/CheckoutCardManagement/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum Constants {
     /// Version of the SDK in use
-    static let productVersion = "1.0.0"
+    static let productVersion = "1.0.1"
     
     /// Product name for logging
     static let productName = "issuing-ios-sdk"
